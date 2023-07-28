15388f4f87e40dc0c01e7232e85b44062d8eaa91
Ex1tar <96546619+ExPressOnePro@users.noreply.github.com>
2023-07-28 08:35:16 +0300

    Load 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,19 +5,18 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Meeper') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- PWA  -->
         <meta name="theme-color" content="#6777ef"/>
         <link rel="apple-touch-icon" href="{{ asset('android-chrome-512x512.PNG') }}">
         <link rel="manifest" href="{{ asset('/manifest.json') }}">
+
         <script src="{{ asset('/sw.js') }}"></script>
 
         <script>
